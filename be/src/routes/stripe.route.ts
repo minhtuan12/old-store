f69a5d413146b50e42465a039df103cc1e148ddf
Nguyễn Minh Tuân <tuannm1.bhsoft@gmail.com>
2025-05-30 16:20:23 +0700

    update payment by stripe

--- be/src/routes/stripe.route.ts
+++ be/src/routes/stripe.route.ts
@@ -4,14 +4,23 @@ import authentication from "../middlewares/authentication";
 
 const stripeRouter = express.Router();
 
-
-
-stripeRouter.get('/account-list', authentication, stripeController.getStripeAccount)
-stripeRouter.post('/create-account', authentication, stripeController.createStripeAccount);
-stripeRouter.get('/account-link/:account_id/:user_id', stripeController.accountLink)
-stripeRouter.get('/login-link', authentication, stripeController.loginLinks);
-stripeRouter.post('/checkout', authentication, stripeController.checkOut);
+stripeRouter.get(
+    "/account-list",
+    authentication,
+    stripeController.getStripeAccount
+);
+stripeRouter.post(
+    "/create-account",
+    authentication,
+    stripeController.createStripeAccount
+);
+stripeRouter.get(
+    "/account-link/:account_id/:user_id",
+    stripeController.accountLink
+);
+stripeRouter.get("/login-link", authentication, stripeController.loginLinks);
+stripeRouter.post("/checkout", authentication, stripeController.checkOut);
 //route to get payment-intent_id if checkout successful
-stripeRouter.get('/payment-success', stripeController.getPaymentIntentId);
+stripeRouter.get("/payment-success", stripeController.getPaymentIntentId);
 
 export default stripeRouter;
